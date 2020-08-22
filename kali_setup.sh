2f754be9b609abebcdb8a16d47d58ee8b613c497
aaa <aaa@kali.aaa.org>
2020-08-22 16:34:07 +0200

    kali

--- kali_setup.sh
+++ kali_setup.sh
@@ -1,2 +1,2 @@
-sudo adduser $USER vboxsf
-printf "alias ECHO=\'echo\'\nalias ALIAS=\'code ~/.bash_aliases\'\nalias RL=\'source ~/.bash_aliases\'\nalias CD=\'code .\'" > ~/.bash_aliases
+# sudo adduser $USER vboxsf
+printf "alias ECHO=\'echo\'\nalias ALIAS=\'code ~/.bash_aliases\'\nalias RL=\'source ~/.bash_aliases\'\nalias CL='reset'\nalias CD=\'code .\'" > /home/$USER/.bash_aliases
