128ef1bc00d64883238ec2c3005eba2bdad3e996
aaa <aaa@gmail.com>
2020-08-22 15:24:22 +0200

    kali

--- kali_setup.sh
+++ kali_setup.sh
@@ -1,1 +1,2 @@
-printf "alias ECHO=\'echo\'\nalias ALIAS=\'code ~/.bash_aliases\'\nalias RL=\'source ~/.bash_aliases\'" > /home/$USER/.bash_aliases
+sudo adduser $USER vboxsf
+printf "alias ECHO=\'echo\'\nalias ALIAS=\'code ~/.bash_aliases\'\nalias RL=\'source ~/.bash_aliases\'\nalias CD=\'code .\'" > ~/.bash_aliases
